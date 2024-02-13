5c33187767cd3d66766560be6efab6985f980f22
kassse1 <124445863+kassse1@users.noreply.github.com>
2024-02-13 23:21:37 +0600

    Delete untitled3 directory

--- untitled3/src/Main.java
+++
@@ -1,8 +0,0 @@
-package University;
-
-    public class Main {
-        public static void main(String[] args) {
-            PhonebookApplication phonebook = new PhonebookApplication();
-            phonebook.run();
-        }
-    }
