418536ec2d39b49fcc6bc93ad995de7d1b2ddb7d
Nicholasssss7 <123289715+Nicholasssss7@users.noreply.github.com>
2024-02-16 11:11:49 +0600

    Delete To-Do-List- directory

--- To-Do-List-/src/main/java/com/example/postgresdemo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.postgresdemo.repository;
-
-import com.example.postgresdemo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-    User findByUsername(String username);
-}
