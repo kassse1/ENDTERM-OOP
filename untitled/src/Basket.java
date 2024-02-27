8c1c128877c82a93f54159c4e7537f2dc5afe25f
kassse1 <124445863+kassse1@users.noreply.github.com>
2024-02-27 21:46:09 +0600

    Add files via upload

---
+++ untitled/src/Basket.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Basket {
+    private ArrayList<Artwork> artworksInBasket;
+    private double basketPrice;
+    public Basket() {
+        this.artworksInBasket = new ArrayList<>();
+        this.basketPrice = 0.0;
+    }
+    public void addArtworkToBasket(Artwork artwork) {
+        artworksInBasket.add(artwork);
+        basketPrice += artwork.getPrice();
+    }
+    public ArrayList<Artwork> getArtworksInBasket() {
+        return new ArrayList<>(artworksInBasket);
+    }
+    public double getBasketPrice() {
+        return basketPrice;
+    }
+}
