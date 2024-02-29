3f133da0a4077053e8718e14f293b7896e5e5a47
kassse1 <124445863+kassse1@users.noreply.github.com>
2024-03-01 01:12:46 +0600

    Delete untitled directory

--- untitled/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-import javax.swing.JFrame;
-public class Main {
-    public static void main(String[] args) {
-        GUI form = new GUI();
-        form.setVisible(true);
-    }
-}
